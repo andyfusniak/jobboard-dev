7050b2153224dc8a279df2b9ace36db5fa1a1128
Namwa <sawasdiworn.k@gmail.com>
2021-10-29 13:06:18 +0100

    Removed -q in rm command

--- delete-old-images.sh
+++ delete-old-images.sh
@@ -2,11 +2,12 @@
 if [ $# -eq 0 ]
   then
     echo 'delete-all-images.sh VERSION'
+    exit 1
 fi
 
 version=$1
 
-docker image rm -q gcr.io/codepyjamas/jobboard-web:$version
-docker image rm -q gcr.io/codepyjamas/jobboard-admin:$version
-docker image rm -q gcr.io/codepyjamas/jobboard-tools:$version
+docker image rm gcr.io/codepyjamas/jobboard-web:$version
+docker image rm gcr.io/codepyjamas/jobboard-admin:$version
+docker image rm gcr.io/codepyjamas/jobboard-tools:$version
 docker image ls
